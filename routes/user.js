36c2670dc4de6eaabb4bf123a834c599d3e982c6
ghaouarbaligh <ghaouarbaligh2020@gmail.com>
2024-07-06 14:50:48 +0100

    third

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const User = require('../models/user.js');
 const jwt = require('jsonwebtoken')
 const multer = require('multer');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+const crypto = require('crypto');
 
 
 
@@ -51,5 +54,59 @@ router.post('/login',async(req,res)=>{
         }
     }
 })
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: true,
+    auth: {
+        user: "balighghaouar@gmail.com",
+        pass: "niat awok ilma rngb"
+    }
+});
+router.post('/forget-password', async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        // Generate a new password
+        const newPassword = crypto.randomBytes(8).toString('hex');
+
+        // Hash the new password before saving it
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        // Update the user's password
+        user.password = hashedPassword;
+        await user.save();
+
+        // Send an email with the new password
+        const mailOptions = {
+            from: process.env.EMAIL,
+            to: email,
+            subject: 'Password Reset',
+            text: `Your new password is: ${newPassword}`
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Error sending email:', error); // Log the error
+                return res.status(500).send('Error sending email');
+            }
+            res.send('Password reset email sent');
+        });
+    } catch (error) {
+        console.error('Internal Server Error:', error); // Log the error
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+
+
+
 
 module.exports = router;
