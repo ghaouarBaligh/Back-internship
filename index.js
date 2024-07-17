94c64fb485913f84affa80258398fd3d690302c7
ghaouarbaligh <ghaouarbaligh2020@gmail.com>
2024-07-17 11:53:43 +0100

    Fix Node.js version in package.json and adjus

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const UserRoute = require('./routes/user.js');
 const VideoRoute = require('./routes/video.js');
 require('./config/connect.js');
 const dotenv = require('dotenv');
+const { default: mongoose } = require('mongoose');
 dotenv.config();
 const app = express();
 app.use(express.json())
+
+mongoose.connect('mongodb+srv://baligh_ghaouar:baligh27169335@cluster0.wvspbku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 //http://127.0.0.1:3000/
 
