e57e57254e506a29bd9a952161df2f5c853fc822
ghaouarbaligh <ghaouarbaligh2020@gmail.com>
2024-07-21 12:06:29 +0100

    connect to mongodb atla

--- config/connect.js
+++ config/connect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
-
-/*mongoose.connect('mongodb://127.0.0.1:27017/app').then(
+require('dotenv').config();
+/*
+mongoose.connect('mongodb://127.0.0.1:27017/app').then(
     ()=>{
         console.log('connected');
     }
@@ -9,6 +10,18 @@ const mongoose = require('mongoose')
     (err)=>{
         console.log(err);
     }
-)*/
+)
+*/
+mongoose.connect('mongodb+srv://baligh_ghaouar:esudInoU7HMYVvEW@cluster0.wvspbku.mongodb.net/app?retryWrites=true&w=majority&appName=Cluster0').then(
+    (conn)=>{
+        console.log(`Database connected: ${conn.connection.host}`);
+    }
+)
+.catch(
+    (err)=>{
+        console.error('Database Error: ${err}');
+        process.exit(1);
+    }
+)
 
 module.exports = mongoose;
